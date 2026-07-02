341b95a0b8c4461ff07732bd8fbc9e2ddc2a8e1f
wewee <altaccout82@gmail.com>
2026-07-02 22:49:36 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function rolldice(){
+
+    const ndice = document.getElementById("ndice").value;
+    const diceresult = document.getElementById("diceresult");
+    const diceimg = document.getElementById("diceimg");
+    const values = [];
+    const imgs = [];
+
+    for(let x = 0; x < ndice; x++){
+        const value = Math.floor(Math.random() * 6) +1;
+        values.push(value);
+        imgs.push(`<img src="Dice/${value}.jpg" alt ="Dice ${value}" style="width:100px; height: 100px; object-fit: cover; border-radiusL 10px;">`);
+    }
+    //The developer kinda slacked off with the design
+
+    diceresult.textContent = `dice: ${values.join(', ')}`;
+    diceimg.innerHTML = imgs.join('');
+}
